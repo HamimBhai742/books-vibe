59e599a03418d231ca2f2ed597be7cbf5f7a2ad0
HamimBhai742 <omagootruelove147@gmail.com>
2024-03-27 23:20:44 +0600

    Page To Read Section Complete

--- src/Componets/PageToRead/PageToRead.jsx
+++ src/Componets/PageToRead/PageToRead.jsx
@@ -6,6 +6,51 @@ import { getReadBooks } from '../../Utility/SaveReadBook';
 
 const colors = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', 'red', 'pink'];
 
+const data = [
+    {
+        name: 'Page A',
+        uv: 4000,
+        pv: 2400,
+        amt: 2400,
+    },
+    {
+        name: 'Page B',
+        uv: 3000,
+        pv: 1398,
+        amt: 2210,
+    },
+    {
+        name: 'Page C',
+        uv: 2000,
+        pv: 9800,
+        amt: 2290,
+    },
+    {
+        name: 'Page D',
+        uv: 2780,
+        pv: 3908,
+        amt: 2000,
+    },
+    {
+        name: 'Page E',
+        uv: 1890,
+        pv: 4800,
+        amt: 2181,
+    },
+    {
+        name: 'Page F',
+        uv: 2390,
+        pv: 3800,
+        amt: 2500,
+    },
+    {
+        name: 'Page G',
+        uv: 3490,
+        pv: 4300,
+        amt: 2100,
+    },
+];
+
 const getPath = (x, y, width, height) => {
     return `M${x},${y + height}C${x + width / 3},${y + height} ${x + width / 2},${y + height / 3}
   ${x + width / 2}, ${y}
@@ -23,6 +68,13 @@ export default function PageToRead() {
     const Books = useLoaderData()
 
     const [readBooks, setReadBooks] = useState([])
+    // const [readBook, setReadBook] = useState([{
+    //     name: '',
+    //     uv: 0,
+    //     pv: 0,
+    //     amt: 0
+    // }])
+
     useEffect(() => {
         const storedBooksId = getReadBooks()
         if (Books.length > 0) {
@@ -35,14 +87,27 @@ export default function PageToRead() {
                 }
             }
             setReadBooks(saveReadBooks)
+            console.log(readBooks);
+            console.log(saveReadBooks);
+            // console.log(readBooks);
         }
     }, [])
 
+    // readBooks.map(r => {
+    //     setReadBook({
+    //         name: r.bookName,
+    //         uv: r.totalPages,
+    //         pv: r.yearOfPublishing
+    //     });
+    //     console.log(r.bookName);
+    // })
+    // const dataBook=useLoaderData()
+    // console.log(dataBook);
     return (
-        <div className='max-w-[1170px] mx-auto'>
+        <div className='max-w-[1170px] mx-auto mt-5'>
             <BarChart
                 width={1170}
-                height={500}
+                height={400}
                 data={readBooks}
                 margin={{
                     top: 20,
